576b147699e612399e5d0c96b226fe198b91c7b0
Stas Sergeev <stsp@users.sourceforge.net>
2024-09-28 12:47:06 +0300

    upstream sync

--- ipxrelay.c
+++ ipxrelay.c
@@ -1,3 +1,30 @@
+/* ----------------------------------------------------------------------- *
+ *
+ *   Copyright 2010-2011 H. Peter Anvin - All Rights Reserved
+ *
+ *   Permission is hereby granted, free of charge, to any person
+ *   obtaining a copy of this software and associated documentation
+ *   files (the "Software"), to deal in the Software without
+ *   restriction, including without limitation the rights to use,
+ *   copy, modify, merge, publish, distribute, sublicense, and/or
+ *   sell copies of the Software, and to permit persons to whom
+ *   the Software is furnished to do so, subject to the following
+ *   conditions:
+ *
+ *   The above copyright notice and this permission notice shall
+ *   be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * ----------------------------------------------------------------------- */
+
 #include <stdbool.h>
 #include <errno.h>
 #include <stdio.h>
@@ -13,6 +40,7 @@
 # include <winsock2.h>
 # include <ws2tcpip.h>
 # include "lib/sysexits.h"
+typedef int socklen_t;
 #else
 # include <sysexits.h>
 # include <unistd.h>
@@ -266,7 +294,7 @@ static int ipx_relay_server(int fd)
 	char buf[65536];
     } pkt;
     struct sockaddr from;
-    int fromlen;
+    socklen_t fromlen;
     int len, hlen;
     struct ipx_client *s, *d;
 
